f6566950b6fc7ad4217edd18eecb1e0ca78eaa3e
flasd <flasd2@gmail.com>
2018-06-04 10:51:05 -0300

    Re-write main login.

--- src/index.js
+++ src/index.js
@@ -1,73 +1,34 @@
-export const DONE_LOADING = '@REDUX_FIRE_AUTH__DONE_LOADING';
-export const AUTH_STATE_CHANGED = '@REDUX_FIRE_AUTH__AUTH_STATE_CHANGED';
+import assert from './assert';
+import {
+    AUTH_STATE_CHANGED,
+    DONE_LOADING,
+    authStateChanged,
+    doneLoading,
+} from './actions';
 
-/**
- *
- * @param {user | null} user The user object
- */
-export function doneLoading(user) {
-    return {
-        type: DONE_LOADING,
-        hasAuth: !!user,
-        user: user && user.toJSON(),
+export default function createAuthMiddleware(authInstance, reducerKey = 'auth') {
+    assert(
+        authInstance && authInstance.onAuthStateChanged,
+        `Expected firebase auth instance instead got ${JSON.stringify(authInstance)}`,
+    );
+
+    assert(
+        Object.prototype.toString.call(reducerKey) === '[object String]',
+        `Expected reducerKey to be a string but instead got ${typeof reducerKey}`,
+    );
+
+    return function authMiddleware(store) {
+        authInstance.onAuthStateChanged((user) => {
+            if (store.getState()[reducerKey].isLoading) {
+                return store.dispatch(doneLoading(user));
+            }
+
+            return store.dispatch(authStateChanged(user));
+        });
+
+        return next => action => next(action);
     };
 }
 
-/**
- *
- * @param {user | null} user The user object
- */
-export function authStateChanged(user) {
-    return {
-        type: AUTH_STATE_CHANGED,
-        hasAuth: !!user,
-        user: user && user.toJSON(),
-    };
-}
-
-export const initialState = {
-    isLoading: true,
-    hasAuth: false,
-    user: null,
-};
-
-/**
- *
- * @param {object} state the redux state
- * @param {action} action the redux action
- */
-export function reduxFireAuthReducer(state = initialState, action) {
-    switch (action.type) {
-        case DONE_LOADING:
-            return Object.assign({}, state, {
-                isLoading: false,
-                hasAuth: action.hasAuth,
-                user: action.user,
-            });
-
-        case AUTH_STATE_CHANGED:
-            return Object.assign({}, state, {
-                hasAuth: action.hasAuth,
-                user: action.user,
-            });
-
-        default:
-            return state;
-    }
-}
-
-/**
- *
- * @param {store} store The redux store,
- * @param {authInstance} authInstance Firebase Auth instance, NOT THE CONSTRUCTOR!!
- * @param {string?} reducerKey The key in witch the reducer was bound with combine reducers.
- */
-export function init(store, authInstance, reducerKey = 'fireAuth') {
-    authInstance.onAuthStateChanged((user) => {
-        if (store.getState()[reducerKey].isLoading) {
-            store.dispatch(doneLoading(user));
-        } else {
-            store.dispatch(authStateChanged(user));
-        }
-    });
-}
+export { AUTH_STATE_CHANGED };
+export { DONE_LOADING };
